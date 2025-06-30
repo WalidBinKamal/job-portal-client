78521e8d281cf73bb6ac80ada9d26da3287595ef
Shinigami0509 <walidbin.kamal64@gmail.com>
2025-07-01 00:08:27 +0600

    private routing

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,6 +6,8 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import SignIn from "../pages/SignIn/SignIn";
+import JobDetails from "../pages/JobDetails/JobDetails";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -18,13 +20,19 @@ const router = createBrowserRouter([
                 element: <Home></Home>
             },
             {
+                path: '/jobs/:id',
+                element: <PrivateRoute><JobDetails></JobDetails></PrivateRoute>,
+                loader: ({ params }) => fetch(`http://localhost:5000/jobs/${params.id}`)
+            },
+            {
                 path: '/register',
                 element: <Register></Register>,
             },
             {
                 path: '/signIn',
                 element: <SignIn></SignIn>
-            }
+            },
+
         ]
     },
 ]);
